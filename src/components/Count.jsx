44451a5c3a169136725759d4c5be677ca411f1cb
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2023-01-05 09:30:50 +0900

    カウントアップ機能を実装した

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,9 +1,35 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Count = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <>
-      <h2>カウント画面</h2>
+      <div className="text-center">
+        <h2>カウント画面</h2>
+        <p>現在の値：{count}</p>
+      </div>
+      <div className="m-4 text-center">
+        <button
+          className="m-4 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          onClick={increment}
+        >
+          +
+        </button>
+        <button
+          className="m-4 bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+          onClick={decrement}
+        >
+          -
+        </button>
+      </div>
     </>
   );
 };
