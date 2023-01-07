8680cd4f75220a02df22af6bc37923a44a8e93f6
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2023-01-07 16:44:37 +0900

    カウントアップ機能で値が0の時に-ボタンを押された際、アラートを表示する機能を実装した。

--- src/components/CountPage.jsx
+++ src/components/CountPage.jsx
@@ -2,13 +2,25 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 export const CountPage = () => {
+  // useStateでカウントする値を管理。初期値は0。
   const [count, setCount] = useState(0);
+  // useStateでアラートを表示を管理。初期値はfalse（表示しない）。
+  const [alert, setAlert] = useState(false);
 
   const increment = () => {
     setCount(count + 1);
+    //   エラーが表示されている状態で+ボタンを押された場合、アラートを非表示にさせる。
+    setAlert(false);
   };
+
   const decrement = () => {
-    setCount(count - 1);
+    if (0 < count) {
+      //   countが0より大きい場合はcountの値から1を引く。
+      setCount(count - 1);
+    } else {
+      // countの値が0の時に-ボタンを押された場合、アラートを表示させる。
+      setAlert(true);
+    }
   };
 
   return (
@@ -31,6 +43,14 @@ export const CountPage = () => {
           -
         </button>
       </div>
+      {/* alertがtrueの場合はアラート文を表示する。falseの場合は空文字を返す。 */}
+      {alert ? (
+        <p className="bg-red-100 border border-red-400 text-red-700 m-4 px-4 py-3 rounded relative">
+          値を0以下にはできません。
+        </p>
+      ) : (
+        ""
+      )}
     </>
   );
 };
