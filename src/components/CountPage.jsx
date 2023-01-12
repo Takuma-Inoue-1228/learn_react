23219dbd2c55c6260f541ca8c7dbbf0c880d6557
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2023-01-12 14:05:39 +0900

    カウント機能をカスタムフックとして切り出し

--- src/components/CountPage.jsx
+++ src/components/CountPage.jsx
@@ -1,27 +1,17 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect } from "react";
+import { useCounter } from "../hooks/useCounter";
 
 export const CountPage = () => {
-  // useStateでカウントする値を管理。初期値は0。
-  const [count, setCount] = useState(0);
-  // useStateでアラートを表示を管理。初期値はfalse（表示しない）。
-  const [alert, setAlert] = useState(false);
+  // カスタムフックとして定義したuseCountを呼び出し、count,alert変数とincrement,decrement関数を呼び出す。
+  const [count, alert, { increment, decrement }] = useCounter();
 
-  const increment = () => {
-    setCount(count + 1);
-    //   エラーが表示されている状態で+ボタンを押された場合、アラートを非表示にさせる。
-    setAlert(false);
-  };
-
-  const decrement = () => {
-    if (0 < count) {
-      //   countが0より大きい場合はcountの値から1を引く。
-      setCount(count - 1);
-    } else {
-      // countの値が0の時に-ボタンを押された場合、アラートを表示させる。
-      setAlert(true);
-    }
-  };
+  // CountPageコンポーネントがマウントされた時とアンマウントされた時にコンスールを表示する。
+  useEffect(() => {
+    console.log("CountPageコンポーネントがマウントされました。");
+    return () => {
+      console.log("CountPageコンポーネントがアンマウントされました。");
+    };
+  }, []);
 
   return (
     <>
---
+++ src/hooks/useCounter.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export const useCounter = () => {
+  // useStateでカウントする値を管理。初期値は0。
+  const [count, setCount] = useState(0);
+  // useStateでアラートを表示を管理。初期値はfalse（表示しない）。
+  const [alert, setAlert] = useState(false);
+
+  const increment = () => {
+    setCount(count + 1);
+    //   エラーが表示されている状態で+ボタンを押された場合、アラートを非表示にさせる。
+    setAlert(false);
+  };
+
+  const decrement = () => {
+    if (0 < count) {
+      //   countが0より大きい場合はcountの値から1を引く。
+      setCount(count - 1);
+    } else {
+      // countの値が0の時に-ボタンを押された場合、アラートを表示させる。
+      setAlert(true);
+    }
+  };
+  // 戻り値としてcount,alert変数とincrement,decrement関数を返す。
+  return [count, alert, { increment, decrement }];
+};
