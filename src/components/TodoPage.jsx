6c5b22d1ab679eacd00a3012625fdba330b4c71b
HeadInoueTakuma <inoue.takuma@pro.headwaters.co.jp>
2023-01-21 07:10:52 +0900

    map関数で箇条書きを出力した。

--- src/components/TodoPage.jsx
+++ src/components/TodoPage.jsx
@@ -1,7 +1,34 @@
+// todoの配列を定義
+const DEFAULT_TODO = [
+  { id: 1, todoTitle: "水を飲む" },
+  { id: 2, todoTitle: "ご飯を食べる" },
+  { id: 3, todoTitle: "片付ける" },
+];
+
+// map関数でイテレートする
+// 定義済みのDEFAULT_TODOオブジェクトのtodoTitleをtodo変数に入れていく
+const todoLists = DEFAULT_TODO.map((todo) => todo.todoTitle);
+console.log(todoLists);
+
+// 新しく配列を定義する
+const array = ["item1", "item2", "item3"];
+// 新しく定義した配列を使ってliタグを付与した配列をを作っていく
+const listItems = array.map((item) => `<li>${item}</li>`);
+console.log(listItems);
+
 export const TodoPage = () => {
   return (
-    <>      
-      <h2>Todo画面</h2>
+    <>
+      <ul>
+        {todoLists.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <ul>
+        {todoLists.map((todo, x) => (
+          <li key={x}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
 };
